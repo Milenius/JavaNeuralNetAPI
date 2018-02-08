a7b063639f11d6d4bae9c22464ffb925b89b4a3e
Milenius <nstrohschen@gmail.com>
2018-02-08 15:02:01 +0100

    Processing Core Implementation

---
+++ test.java
@@ -0,0 +1,19 @@
+import processing.core.PApplet;
+
+class test extends PApplet{
+  public static void main(String[] args) {
+    PApplet.main("test");
+  }
+
+  public void settings(){
+    size(1600,900);
+  }
+
+  public void setup(){
+
+  }
+
+  public void draw(){
+    ellipse(50,50,50,50);
+  }
+}
