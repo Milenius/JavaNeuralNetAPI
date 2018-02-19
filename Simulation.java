edc5d6a05c39610c1e8f0b7a2b2950dab1f38ff8
Milenius <nstrohschen@gmail.com>
2018-02-19 11:05:48 +0100

    Update Simulation.java

--- Simulation.java
+++ Simulation.java
@@ -3,17 +3,22 @@ class Simulation {
 
     //Currently just testing area
     NeuralNet net = new NeuralNet(3, new int[]{8, 7}, 4);
-    Matrix testInput = new Matrix(new float[][]{{0.3f},{0.6f},{0.9f}});
+    Matrix trainInput = new Matrix(new float[][]{{0.0f, 0.0f},
+                                                 {0.0f, 1.0f},
+                                                 {1.0f, 0.0f},
+                                                 {1.0f, 1.0f}});
+
+    Matrix trainOutput = new Matrix(new float[][]{{0.0f},
+                                                  {1.0f},
+                                                  {1.0f},
+                                                  {0.0f}});
 
     net.randomizeSynapses();
-    //net.setGeneralActivationFunc("sigmoid");
-    net.feedForward(testInput);
+    net.feedForward(trainInput);
 
     for (int i = 0; i < net.layer.length; i++) {
-      System.out.println("");
       net.layer[i].neurons.print();
-      //net.syns[i].print();
     }
-    //NeuronLayer.sigmoid(Matrix.matMul(net.syns[0], net.layer[0].getValues())).print();
+
   }
 }
